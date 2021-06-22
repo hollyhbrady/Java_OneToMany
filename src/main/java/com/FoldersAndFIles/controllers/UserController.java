8dca7e9a2f8e22c839d37dfd3e7a83266a7c9929
hollyhbrady <hollyhbrady@gmail.com>
2021-06-22 19:29:53 +0100

    controllers

---
+++ src/main/java/com/FoldersAndFIles/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.FoldersAndFIles.controllers;
+
+import com.FoldersAndFIles.models.User;
+import com.FoldersAndFIles.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity getUser(@PathVariable Long id){
+        return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<User> postUser(@RequestBody User user){
+        return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+    }
+}
